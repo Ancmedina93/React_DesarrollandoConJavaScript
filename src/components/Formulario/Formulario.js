c743194985686c1de9803f03668c157e95356566
Ancmedina93 <ancm93@gmail.com>
2023-05-22 11:54:49 -0500

    Clase 04 React

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -4,9 +4,15 @@ import ListaOpciones from "../ListaOpciones";
 import Boton from "../Bonton";
 
 const Formulario = () => {
+
+    const manejarEnvio = (event) => {
+        event.preventDefault();
+        console.log("Manejar el envio", event);
+    }
+
     return( 
     <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>
                 Rellena el formulario para crear el colaborador.
             </h2>
@@ -14,9 +20,7 @@ const Formulario = () => {
             <CampoTexto titulo = "Puesto" placeholder = "Ingresar Puesto"/>
             <CampoTexto titulo = "Foto" placeholder = "Ingresar enlace de foto"/>
             <ListaOpciones />
-            <Boton>
-                Crear
-            </Boton>
+            <Boton texto="Crear"/>
         </form>
     </section>
     )
